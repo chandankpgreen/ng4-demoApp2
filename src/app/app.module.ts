8783b9ac4e626a18c67e0c5ba2b56267a124ef60
chandank <chandank@meditab.com>
2018-05-29 15:21:15 +0530

    Exercise 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-Alert/warningAlert.component';
+import { SuccessAlertComponent } from './success-Alert/successAlert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
